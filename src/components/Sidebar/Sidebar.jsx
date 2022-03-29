8b1b176ece89d6f8f241e212a8d60572f90440d0
Temurbek Rajapboyev <rajapboyevtemurbek@gmail.com>
2022-03-29 11:14:28 +0500

    cors

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Friend from "./Friend/Friend";
-import SidebarCSS from "./Sidebar.module.css"
-
+import SidebarCSS from "./Sidebar.module.css";
 
 const Sidebar = (props) => {
-
-    return (
-      <div className={SidebarCSS.sidebar}> 
-        <h3>Followed users</h3>
-        {props.followedUsers.map((user, index) => {
-          if (index <= 10) {
-            return <Friend {...user}/>
-          }
-        })}
+   return (
+      <div className={SidebarCSS.sidebar}>
+         <h3>Followed users</h3>
+         {props.followedUsers.map((user, index) => {
+            if (index <= 10) {
+               return <Friend {...user} />;
+            }
+            return "";
+         })}
       </div>
-    )
-}
+   );
+};
 
 export default Sidebar;
